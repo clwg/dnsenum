ff5331770e47870944d8d265befb0299e93a851e
Chris <chris@layer8.org>
2024-07-31 11:51:32 -0400

    removed non-implemented records

--- pkg/dnsrecord/dnsrecord.go
+++ pkg/dnsrecord/dnsrecord.go
@@ -5,7 +5,6 @@ type DNSRecords struct {
 	MX    []MXRecord    `json:"mx"`
 	NS    []NSRecord    `json:"ns"`
 	A     []ARecord     `json:"a"`
-	SPF   []string      `json:"spf"`
 	TXT   []TXTRecord   `json:"txt"`
 	SRV   []SRVRecord   `json:"srv"`
 	CNAME []CNAMERecord `json:"cname"`
